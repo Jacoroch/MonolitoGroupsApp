95e4110465a4d708eba5d86220a0d28a9b72c7a4
josedtf <josedavidtorofranco@gmail.com>
2026-03-19 14:12:08 -0500

    agregar login, signup, index y chats

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
 from core.database import engine, Base
 
-# --- Importación de modelos (SQLAlchemy DDL) ---
+# modelos
 from modules.auth import models as auth_models
-from modules.groups import models as groups_models         
-from modules.messaging import models as messaging_models   
+from modules.groups import models as groups_models
+from modules.messaging import models as messaging_models
 
-# --- Importación de enrutadores (Routers) ---
+# routers
 from modules.auth.router import router as auth_router
 from modules.groups.router import router as groups_router
 from modules.messaging.router import router as messaging_router
@@ -14,15 +17,28 @@ from modules.messaging.router import router as messaging_router
 # Ejecución DDL: Crea las tablas si no existen en PostgreSQL
 Base.metadata.create_all(bind=engine)
 
-# Instanciación de la aplicación FastAPI
 app = FastAPI(title="GroupsApp API", version="1.0")
 
-# --- Registro de sub-aplicaciones (Routers) ---
+# configurar templates
+templates = Jinja2Templates(directory="templates")
+
+# routers
 app.include_router(auth_router)
 app.include_router(groups_router)
-app.include_router(messaging_router)  # Integración del nuevo módulo WebSocket
+app.include_router(messaging_router)
 
-# --- Endpoint REST de verificación de estado (Health Check) ---
-@app.get("/")
-async def root():
-    return {"mensaje": "¡El monolito de GroupsApp está en línea y la base de datos está conectada!"}
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/login", response_class=HTMLResponse)
+def login_view(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+@app.get("/signup", response_class=HTMLResponse)
+def signup_view(request: Request):
+    return templates.TemplateResponse("signup.html", {"request": request})
+
+@app.get("/chats", response_class=HTMLResponse)
+def chats_view(request: Request):
+    return templates.TemplateResponse("chats.html", {"request": request})
